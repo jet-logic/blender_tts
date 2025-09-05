980434c172be622d5da14352cc88d4b83a528fbd
jet-logic <209652123+jet-logic@users.noreply.github.com>
2025-09-05 15:36:32 +0800

    fix makezip.sh

--- makezip.sh
+++ makezip.sh
@@ -2,8 +2,8 @@
 # from tts_narration import bl_info
 # print(bl_info["version"])
 # EOF
-
-ZIP=$(realpath dist/tts_narration.zip)
 mkdir -p dist
+ZIP=$(realpath dist/tts_narration.zip)
+
 rm -fv $ZIP
-zip -r $ZIP tts_narration -i '*.py'
+zip -r "$ZIP" tts_narration -i '*.py'
